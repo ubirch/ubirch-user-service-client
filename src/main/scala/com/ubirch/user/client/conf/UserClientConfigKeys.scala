7cb38a83e33684c237262bb1766f0b3634f9fe6e
lotta <lotta.rueger@ubirch.com>
2020-07-31 11:20:02 +0200

    removing not needed config keys

--- src/main/scala/com/ubirch/user/client/conf/UserClientConfigKeys.scala
+++ src/main/scala/com/ubirch/user/client/conf/UserClientConfigKeys.scala
@@ -1,7 +1,6 @@
 package com.ubirch.user.client.conf
 
 import java.net.URLEncoder
-import java.util.UUID
 
 object UserClientConfigKeys {
 
@@ -15,40 +14,21 @@ object UserClientConfigKeys {
 
   final val check = "check"
   final val deepCheck = "deepCheck"
-
-  final val context = "context"
-  final val byName = "byName"
   final val user = "user"
   final val group = "group"
-  final val allowedUsers = "allowedUsers"
   final val memberOf = "memberOf"
-  final val initData = "initData"
-
   final val externalIdExists = "extIdExists"
   final val recreate = "recreate"
   final val info = "info"
-
   final val register = "register"
-
   val pathPrefix = s"/$apiPrefix/$serviceName/$currentVersion"
-
   val pathCheck = s"$pathPrefix/$check"
   val pathDeepCheck = s"$pathPrefix/$deepCheck"
-
-  val pathContext = s"$pathPrefix/$context"
-
-  def pathContextWithId(id: UUID) = s"$pathContext/$id"
-
-  def pathContextFindByName(name: String): String = {
-
-    val encodedName = URLEncoder.encode(name, "UTF-8")
-
-    s"$pathContext/$byName/$encodedName"
-
-  }
-
   val pathUser = s"$pathPrefix/$user"
   val pathRecreate = s"$pathPrefix/$user/$recreate"
+  final val pathUserInfoPUT = pathUserInfo
+  final val pathRegister = s"$pathPrefix/$register"
+  private val pathUserInfo = s"$pathUser/$info"
 
   def pathUserFind(providerId: String, externalUserId: String): String = {
 
@@ -84,12 +64,6 @@ object UserClientConfigKeys {
 
   }
 
-  val pathGroup = s"$pathPrefix/$group"
-
-  def pathGroupWithId(id: UUID) = s"$pathGroup/$id"
-
-  val pathGroupAllowedUsers = s"$pathGroup/$allowedUsers"
-
   def pathGroupMemberOf(contextName: String,
                         providerId: String,
                         externalUserId: String
@@ -103,11 +77,6 @@ object UserClientConfigKeys {
 
   }
 
-  final def pathInitData(env: String) = s"$pathPrefix/$initData/$env"
-
-  final val pathRegister = s"$pathPrefix/$register"
-
-  private val pathUserInfo = s"$pathUser/$info"
   final def pathUserInfoGET(context: String, providerId: String, userId: String): String = {
 
     val contextEncoded = URLEncoder.encode(context, "UTF-8")
@@ -115,9 +84,7 @@ object UserClientConfigKeys {
     val userEncoded = URLEncoder.encode(userId, "UTF-8")
 
     s"$pathUserInfo/$contextEncoded/$providerEncoded/$userEncoded"
-
   }
-  final val pathUserInfoPUT = pathUserInfo
 
 
 }
